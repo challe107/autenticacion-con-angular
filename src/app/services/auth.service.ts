a4df66bc1e7fe3d27f756c3973cc025f4f818878
Carlos Valladares <cvalladares@canvia.com>
2023-02-24 17:59:08 -0500

    Clase 09

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment'
-import { switchMap } from 'rxjs/operators';
+import { ResponseLogin } from '@models/auth.model';
+import { switchMap, tap } from 'rxjs/operators';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +13,22 @@ export class AuthService {
   apiUrl = environment.API_URL;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private tokenService: TokenService,
   ) { }
 
   login(email: string, password: string) {
-    return this.http.post(`${ this.apiUrl }/api/v1/auth/login`, {
+    return this.http.post<ResponseLogin>(`${ this.apiUrl }/api/v1/auth/login`, {
       email,
       password
-    });
+    })
+    .pipe(
+      tap(response => {
+        if (response) {
+          this.tokenService.saveToken(response.access_token);
+        }
+      })
+    );
   }
 
   register(name: string, email: string, password: string) {
