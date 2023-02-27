4bff25c610d360ea0a7888ca89d73b0d8daa628c
Carlos Valladares <cvalladares@canvia.com>
2023-02-27 14:01:54 -0500

    Clase 19 y Clase 20

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -40,6 +40,20 @@ export class AuthService {
     );
   }
 
+  refreshToken(refreshToken: string) {
+    return this.http.post<ResponseLogin>(`${ this.apiUrl }/api/v1/auth/refresh-token`, {
+      refreshToken
+    })
+    .pipe(
+      tap(response => {
+        if (response) {
+          this.tokenService.saveToken(response.access_token);
+          this.tokenService.saveRefreshToken(response.refresh_token);
+        }
+      })
+    );
+  }
+
   register(name: string, email: string, password: string) {
     return this.http.post(`${ this.apiUrl }/api/v1/auth/register`, {
       name,
